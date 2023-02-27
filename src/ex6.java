1548c06bfb9c514f171aa09a308d8db55c1b372f
lilika1 <kanyangeliliane@gmail.com>
2023-02-27 09:25:45 +0100

    3 commit

--- src/ex6.java
+++ src/ex6.java
@@ -2,5 +2,30 @@ import java.util.Scanner;
 public class ex6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //int pnum = 0;
+        int psum = 0;
+        int avg = 0;
+        int nsum = 0;
+        int ncount=0;
+
+
+        for (int i = 1; i < 10; i++) {
+            System.out.print("enter integers: ");
+            int num = input.nextInt();
+
+            if (num > 0) {
+                psum += num;
+            } else {
+                ncount++;
+                nsum += num;
+
+                avg = nsum / ncount;
+
+            }
+
+
+        }
+        System.out.print("posit sum is:" + psum +  "\n"+ "negat avg is:"+avg);
+
     }
 }
