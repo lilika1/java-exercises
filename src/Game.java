f8c49c0ecced57a5bb588a4972f6d8c23d5b61fb
lilika1 <kanyangeliliane@gmail.com>
2023-02-28 13:12:41 +0100

    first commit

---
+++ src/Game.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Game {
+    private Player[] players;
+
+    private int currentPlayer;
+
+
+    public Game(String[] playerNames) {
+        players = new Player[playerNames.length];
+        for (int i = 0; i < playerNames.length; i++) {
+            players[i] = new Player(playerNames[i]);
+        }
+        currentPlayer = 0;
+    }
+    public void play(){
+        Random dice = new Random();
+        while(players[currentPlayer].getPosition() < 63) {
+            int roll = dice.nextInt(6) + dice.nextInt(6) + 2;
+            System.out.println(players[currentPlayer].getName() + "rolled a "+ roll);
+            players[currentPlayer].move(roll);
+
+        }
+        System.out.println(players[currentPlayer].getName() + " won the game!");
+    }
+
+}
