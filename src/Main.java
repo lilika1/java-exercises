be9cb40458e724786caef6ed8d117fc1e2ecd119
lilika1 <kanyangeliliane@gmail.com>
2023-02-28 12:29:47 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String playerNames [] = {"Player 1","Player 2","Player 3"};
+        Game game = new Game(playerNames);
+        game.play();
     }
 }
